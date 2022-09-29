78b8d24669eebd4149ca991dd0c12c958914644c
DenisPeleng <rangerby@icloud.com>
2022-09-29 16:23:36 +0400

    Completed Stage 3

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -3,36 +3,63 @@ package numbers;
 import java.util.Scanner;
 
 public class Main {
+    private static boolean isExit = false;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        int number = scanner.nextInt();
-        printProperties(number);
+        System.out.print("""
+                Welcome to Amazing Numbers!
+
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter 0 to exit.
+                """);
+
+        while (!isExit) {
+            showMenu();
+        }
+
     }
 
-    public static boolean isDivisableBySeven(int number) {
-        int smallestDivider = number;
+    public static void showMenu() {
+        System.out.println("");
+        System.out.print("Enter a request: ");
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextLong();
+        System.out.println("");
+        if (isNatural(number)) {
+            printProperties(number);
+        } else if (number == 0) {
+            isExit = true;
+            System.out.println("Goodbye!");
+        } else {
+            System.out.println("The first parameter should be a natural number or zero.");
+        }
+
+    }
+
+    private static boolean isDivisableBySeven(long number) {
+        long smallestDivider = number;
         while (smallestDivider > 10) {
-            int lastDigit = smallestDivider % 10;
-            int firstPart = smallestDivider / 10;
+            long lastDigit = smallestDivider % 10;
+            long firstPart = smallestDivider / 10;
             smallestDivider = Math.abs(firstPart - lastDigit * 2);
         }
         return smallestDivider % 7 == 0;
     }
 
-    public static boolean isEndedBySeven(int number) {
+    private static boolean isEndedBySeven(long number) {
         return number % 10 == 7;
     }
 
-    public static boolean isOddNumber(int number) {
+    private static boolean isOddNumber(long number) {
         return number % 2 != 0;
     }
 
-    public static boolean isBuzzNumber(int number) {
+    private static boolean isBuzzNumber(long number) {
         return isDivisableBySeven(number) || isEndedBySeven(number);
     }
 
-    public static void printResultsWithExplanation(int number) {
+    private static void printResultsWithExplanation(long number) {
         boolean isDivisableBySeven = isDivisableBySeven(number);
         boolean isEndedBySeven = isEndedBySeven(number);
         boolean isOddNumber = isOddNumber(number);
@@ -62,19 +89,30 @@ public class Main {
         }
     }
 
-    public static void printProperties(int number) {
-        if (number <= 0) {
-            System.out.println("This number is not natural!");
-            return;
-        }
-        System.out.println("Properties of " + number);
+    private static void printProperties(long number) {
+        System.out.printf("Properties of %d%n", number);
         System.out.println("        even: " + !isOddNumber(number));
         System.out.println("         odd: " + isOddNumber(number));
         System.out.println("        buzz: " + isBuzzNumber(number));
         System.out.println("        duck: " + isDuckNumber(number));
+        System.out.println(" palindromic: " + isPalindromic(number));
     }
 
-    private static boolean isDuckNumber(int number) {
+    private static boolean isPalindromic(long number) {
+        char[] numberInCharArr = String.valueOf(number).toCharArray();
+        for (int i = 0; i < numberInCharArr.length / 2; i++) {
+            if (numberInCharArr[i] != numberInCharArr[numberInCharArr.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNatural(long number) {
+        return number > 0;
+    }
+
+    private static boolean isDuckNumber(long number) {
         while (number >= 10) {
             if (number % 10 == 0) {
                 return true;
