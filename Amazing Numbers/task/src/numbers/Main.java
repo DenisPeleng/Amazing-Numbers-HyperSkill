a79764467783c228f0d6a3cc3eedbddf939fb468
DenisPeleng <rangerby@icloud.com>
2022-10-01 23:31:52 +0400

    Completed Stage 6

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -6,23 +6,26 @@ public class Main {
     private static boolean isExit = false;
 
     public static void main(String[] args) {
-        System.out.print("""
-                Welcome to Amazing Numbers!
+        showGreeting();
+        while (!isExit) {
+            showMenu();
+        }
 
+    }
+
+    private static void showGreeting() {
+        System.out.println(" Welcome to Amazing Numbers!");
+        System.out.print("""
                 Supported requests:
                 - enter a natural number to know its properties;
                 - enter two natural numbers to obtain the properties of the list:
                   * the first parameter represents a starting number;
                   * the second parameter shows how many consecutive numbers are to be printed;
                 - two natural numbers and a property to search for;
+                - two natural numbers and two properties to search for;
                 - separate the parameters with one space;
                 - enter 0 to exit.
                                 """);
-
-        while (!isExit) {
-            showMenu();
-        }
-
     }
 
     public static void showMenu() {
@@ -31,256 +34,143 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String[] strArrWithNumbers = scanner.nextLine().split(" ");
         System.out.println();
-        switch (strArrWithNumbers.length) {
-            case 1 -> printPropertiesOfNumber(strArrWithNumbers);
-            case 2 -> printPropertiesOfRangeNumbers(strArrWithNumbers);
-            case 3 -> printSearchPropertiesOfRange(strArrWithNumbers);
-            default -> System.out.println("Wrong Amount of arguments");
+        if (isCorrectArguments(strArrWithNumbers)) {
+            switch (strArrWithNumbers.length) {
+                case 1 -> printPropertiesForOneLarge(strArrWithNumbers);
+                case 2 -> printPropertiesOfRangeNumbers(strArrWithNumbers);
+                case 3 -> printSearchPropertiesOfRange(strArrWithNumbers);
+                case 4 -> printSearchWithTwoPropertiesOfRange(strArrWithNumbers);
+                default -> System.out.println("Wrong Amount of arguments");
+            }
         }
 
     }
 
-    private static void printSearchPropertiesOfRange(String[] strArrWithNumbers) {
-        long number = 0;
-        long counter = 0;
-        try {
-            number = Long.parseLong(strArrWithNumbers[0]);
-            if (!isNatural(number)) {
-                throw new NumberFormatException();
-            }
 
-        } catch (NumberFormatException e) {
-            System.out.println("The first parameter should be a natural number or zero.");
+    private static void printSearchWithTwoPropertiesOfRange(String[] strArrWithNumbers) {
+        long number = Long.parseLong(strArrWithNumbers[0]);
+        long countNumbers = Long.parseLong(strArrWithNumbers[1]);
+        String property1 = strArrWithNumbers[2].toLowerCase();
+        String property2 = strArrWithNumbers[3].toLowerCase();
+        String strArgs = String.join(" ", strArrWithNumbers).toLowerCase();
+        if ((strArgs.contains("even") && strArgs.contains("odd")
+                || strArgs.contains("sunny") && strArgs.contains("square"))
+                || (strArgs.contains("spy") && strArgs.contains("duck"))) {
+            System.out.println("The request contains mutually exclusive properties: ");
+            System.out.printf("[%s, %s]%n", property1.toUpperCase(), property2.toUpperCase());
+            System.out.println("There are no numbers with these properties.");
+            return;
         }
-        try {
-            counter = Long.parseLong(strArrWithNumbers[1]);
-            if (!isNatural(counter)) {
-                throw new NumberFormatException();
+        int currentAmount = 0;
+        while (countNumbers > currentAmount) {
+            NumberWithProperties currentNumberWithProperties = new NumberWithProperties(number);
+            if (currentNumberWithProperties.properties.contains(property1) && currentNumberWithProperties.properties.contains(property2)) {
+                printPropertiesForOneShort(currentNumberWithProperties);
+                currentAmount++;
             }
-        } catch (NumberFormatException e) {
-            System.out.println("The second parameter should be a natural number");
+            number++;
         }
-        String property = strArrWithNumbers[2];
-
-        searchAndPrintByProperty(number, counter, property);
     }
 
-    private static void printPropertiesOfRangeNumbers(String[] strArrWithNumbers) {
-        long firstNumber = 0;
+    private static boolean isCorrectArguments(String[] strArrWithNumbers) {
+        List<String> propertiesNames = Arrays.asList("BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY", "SQUARE", "SUNNY", "EVEN", "ODD");
+        long firstNumber;
         long secondNumber;
         try {
             firstNumber = Long.parseLong(strArrWithNumbers[0]);
-            if (!isNatural(firstNumber)) {
+            if (isNotNaturalNumber(firstNumber) && firstNumber != 0) {
                 throw new NumberFormatException();
-            }
-
-        } catch (NumberFormatException e) {
-            System.out.println("The first parameter should be a natural number or zero.");
-        }
-        try {
-            secondNumber = Long.parseLong(strArrWithNumbers[1]);
-            if (isNatural(secondNumber)) {
-                printPropertiesForArray(firstNumber, secondNumber);
-            } else {
-                throw new NumberFormatException();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("The second parameter should be a natural number");
-        }
-
-
-    }
-
-    private static void printPropertiesOfNumber(String[] strArrWithNumbers) {
-        long firstNumber;
-        try {
-            firstNumber = Long.parseLong(strArrWithNumbers[0]);
-            if (isNatural(firstNumber)) {
-
-                printPropertiesForOneLarge(firstNumber);
             } else if (firstNumber == 0) {
                 isExit = true;
                 System.out.println("Goodbye!");
-            } else {
-                throw new NumberFormatException();
+                return false;
             }
-
         } catch (NumberFormatException e) {
             System.out.println("The first parameter should be a natural number or zero.");
+            return false;
         }
+        if (strArrWithNumbers.length > 1) {
+            try {
+                secondNumber = Long.parseLong(strArrWithNumbers[1]);
+                if (isNotNaturalNumber(secondNumber)) {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("The second parameter should be a natural number");
+                return false;
+            }
+            if (strArrWithNumbers.length > 2) {
+                int indexOfWrongArgument = -1;
+                for (int i = 2; i < strArrWithNumbers.length; i++) {
+                    if (!propertiesNames.contains(strArrWithNumbers[i].toUpperCase())) {
+                        indexOfWrongArgument = indexOfWrongArgument == -1 ? i : -2;
+                    }
+                }
 
-    }
-
-    private static boolean isDivisableBySeven(long number) {
-        long smallestDivider = number;
-        while (smallestDivider > 10) {
-            long lastDigit = smallestDivider % 10;
-            long firstPart = smallestDivider / 10;
-            smallestDivider = Math.abs(firstPart - lastDigit * 2);
-        }
-        return smallestDivider % 7 == 0;
-    }
-
-    private static boolean isEndedBySeven(long number) {
-        return number % 10 == 7;
-    }
-
-    private static boolean isOddNumber(long number) {
-        return number % 2 != 0;
-    }
-
-    private static boolean isBuzzNumber(long number) {
-        return isDivisableBySeven(number) || isEndedBySeven(number);
-    }
-
-    private static void printPropertiesForOneLarge(long number) {
-        System.out.printf("Properties of %d%n", number);
-        System.out.println("        even: " + !isOddNumber(number));
-        System.out.println("         odd: " + isOddNumber(number));
-        System.out.println("        buzz: " + isBuzzNumber(number));
-        System.out.println("        duck: " + isDuckNumber(number));
-        System.out.println(" palindromic: " + isPalindromicNumber(number));
-        System.out.println("      gapful: " + isGapfulNumber(number));
-        System.out.println("         spy: " + isSpyNumber(number));
-    }
-
-    private static boolean isPalindromicNumber(long number) {
-        char[] numberInCharArr = String.valueOf(number).toCharArray();
-        for (int i = 0; i < numberInCharArr.length / 2; i++) {
-            if (numberInCharArr[i] != numberInCharArr[numberInCharArr.length - i - 1]) {
+                if (indexOfWrongArgument == -2) {
+                    System.out.printf("The properties [%s, %s] are wrong.%n", strArrWithNumbers[2].toUpperCase(), strArrWithNumbers[3].toUpperCase());
+                } else if (indexOfWrongArgument == -1) {
+                    return true;
+                } else {
+                    System.out.printf("The property [%s] is wrong.%n", strArrWithNumbers[indexOfWrongArgument].toUpperCase());
+                }
+                System.out.printf("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]%n");
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean isNatural(long number) {
-        return number > 0;
-    }
-
-    private static boolean isDuckNumber(long number) {
-        while (number >= 10) {
-            if (number % 10 == 0) {
-                return true;
+    private static void printSearchPropertiesOfRange(String[] strArrWithNumbers) {
+        long number = Long.parseLong(strArrWithNumbers[0]);
+        long counter = Long.parseLong(strArrWithNumbers[1]);
+        String property = strArrWithNumbers[2];
+        int currentAmount = 0;
+        while (counter > currentAmount) {
+            NumberWithProperties currentNumberWithProperties = new NumberWithProperties(number);
+            if (currentNumberWithProperties.properties.contains(property.toLowerCase())) {
+                printPropertiesForOneShort(currentNumberWithProperties);
+                currentAmount++;
             }
-            number = number / 10;
-        }
-        return false;
-    }
-
-    private static boolean isGapfulNumber(long number) {
-        if (number / 100 > 0) {
-            long firstDigit = number;
-            long lastDigit = number % 10;
-            while (firstDigit >= 10) {
-                firstDigit = firstDigit / 10;
-            }
-            String remainderForGapful = firstDigit + String.valueOf(lastDigit);
-            return number % (Long.parseLong(remainderForGapful)) == 0;
-        }
-        return false;
-    }
-
-    private static boolean isSpyNumber(long number) {
-        long multiplyDigits = 1;
-        long sumDigits = 0;
-        long currentPartNumber = number;
-        while (currentPartNumber >= 10) {
-            long digit = currentPartNumber % 10;
-            multiplyDigits *= digit;
-            sumDigits += digit;
-            currentPartNumber = currentPartNumber / 10;
-        }
-        long digit = currentPartNumber % 10;
-        multiplyDigits *= digit;
-        sumDigits += digit;
-        return sumDigits == multiplyDigits;
-    }
-
-    private static void printPropertiesForArray(long number, long count) {
-        for (int i = 0; i < count; i++) {
-            printPropertiesForOneShort(number);
             number++;
         }
     }
 
-    private static void printPropertiesForOneShort(long number) {
-        List<String> listOfNumberProperties = new ArrayList<>();
-        listOfNumberProperties.add(isOddNumber(number) ? "odd" : "even");
-        if (isBuzzNumber(number)) {
-            listOfNumberProperties.add("buzz");
+    private static void printPropertiesOfRangeNumbers(String[] strArrWithNumbers) {
+        long number = Long.parseLong(strArrWithNumbers[0]);
+        long countNumbers = Long.parseLong(strArrWithNumbers[1]);
+        for (int i = 0; i < countNumbers; i++) {
+            NumberWithProperties currentNumberWithProperties = new NumberWithProperties(number);
+            printPropertiesForOneShort(currentNumberWithProperties);
+            number++;
         }
-        if (isDuckNumber(number)) {
-            listOfNumberProperties.add("duck");
-        }
-        if (isPalindromicNumber(number)) {
-            listOfNumberProperties.add("palindromic");
-        }
-        if (isGapfulNumber(number)) {
-            listOfNumberProperties.add("gapful");
-        }
-        if (isSpyNumber(number)) {
-            listOfNumberProperties.add("spy");
-        }
+
+    }
+
+    private static void printPropertiesForOneLarge(String[] strArrWithNumbers) {
+        long number = Long.parseLong(strArrWithNumbers[0]);
+        NumberWithProperties currentNumber = new NumberWithProperties(number);
+        System.out.printf("Properties of %d%n", number);
+        System.out.println("        even: " + currentNumber.properties.contains("even"));
+        System.out.println("         odd: " + currentNumber.properties.contains("odd"));
+        System.out.println("        buzz: " + currentNumber.properties.contains("buzz"));
+        System.out.println("        duck: " + currentNumber.properties.contains("duck"));
+        System.out.println(" palindromic: " + currentNumber.properties.contains("palindromic"));
+        System.out.println("      gapful: " + currentNumber.properties.contains("gapful"));
+        System.out.println("         spy: " + currentNumber.properties.contains("spy"));
+        System.out.println("       sunny: " + currentNumber.properties.contains("sunny"));
+        System.out.println("      square: " + currentNumber.properties.contains("square"));
+    }
+
+    private static boolean isNotNaturalNumber(long number) {
+        return number <= 0;
+    }
+
+    private static void printPropertiesForOneShort(NumberWithProperties currentNumber) {
+        List<String> listOfNumberProperties = currentNumber.properties;
         Collections.sort(listOfNumberProperties);
         String propertiesString = String.join(", ", listOfNumberProperties);
-        System.out.printf("        %s is %s%n", number, propertiesString);
+        System.out.printf("        %s is %s%n", currentNumber.number, propertiesString);
     }
 
-    private static void searchAndPrintByProperty(long number, long countNumbers, String property) {
-        int currentAmount = 0;
-        while (countNumbers > currentAmount) {
-            switch (property.toUpperCase()) {
-                case "BUZZ" -> {
-                    if (isBuzzNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                case "DUCK" -> {
-                    if (isDuckNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                case "PALINDROMIC" -> {
-                    if (isPalindromicNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-
-                    }
-                }
-                case "GAPFUL" -> {
-                    if (isGapfulNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                case "SPY" -> {
-                    if (isSpyNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                case "EVEN" -> {
-                    if (!isOddNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                case "ODD" -> {
-                    if (isOddNumber(number)) {
-                        printPropertiesForOneShort(number);
-                        currentAmount++;
-                    }
-                }
-                default -> {
-                    System.out.printf("The property [%s] is wrong.%n", property);
-                    System.out.printf("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]%n");
-                    return;
-                }
-            }
-            number++;
-        }
-    }
 }
---
+++ Amazing Numbers/task/src/numbers/NumberWithProperties.java
@@ -0,0 +1,117 @@
+package numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberWithProperties {
+    long number;
+    List<String> properties = new ArrayList<>();
+
+    NumberWithProperties(long number) {
+        this.number = number;
+        if (isBuzzNumber(number)) {
+            properties.add("buzz");
+        }
+        if (isDuckNumber(number)) {
+            properties.add("duck");
+        }
+        if (isPalindromicNumber(number)) {
+            properties.add("palindromic");
+        }
+        if (isGapfulNumber(number)) {
+            properties.add("gapful");
+        }
+        if (isSpyNumber(number)) {
+            properties.add("spy");
+        }
+        if (isSunnyNumber(number)) {
+            properties.add("sunny");
+        }
+        if (isSquareNumber(number)) {
+            properties.add("square");
+        }
+        properties.add(isOddNumber(number) ? "odd" : "even");
+    }
+
+    private static boolean isSunnyNumber(long number) {
+        return isSquareNumber(number + 1);
+    }
+
+    private static boolean isSquareNumber(long number) {
+
+        return Math.sqrt(number) % 1 == 0;
+    }
+
+    private static boolean isDuckNumber(long number) {
+        while (number >= 10) {
+            if (number % 10 == 0) {
+                return true;
+            }
+            number = number / 10;
+        }
+        return false;
+    }
+
+    private static boolean isGapfulNumber(long number) {
+        if (number / 100 > 0) {
+            long firstDigit = number;
+            long lastDigit = number % 10;
+            while (firstDigit >= 10) {
+                firstDigit = firstDigit / 10;
+            }
+            String remainderForGapful = firstDigit + String.valueOf(lastDigit);
+            return number % (Long.parseLong(remainderForGapful)) == 0;
+        }
+        return false;
+    }
+
+    private static boolean isSpyNumber(long number) {
+        long multiplyDigits = 1;
+        long sumDigits = 0;
+        long currentPartNumber = number;
+        while (currentPartNumber >= 10) {
+            long digit = currentPartNumber % 10;
+            multiplyDigits *= digit;
+            sumDigits += digit;
+            currentPartNumber = currentPartNumber / 10;
+        }
+        long digit = currentPartNumber % 10;
+        multiplyDigits *= digit;
+        sumDigits += digit;
+        return sumDigits == multiplyDigits;
+    }
+
+    private static boolean isPalindromicNumber(long number) {
+        char[] numberInCharArr = String.valueOf(number).toCharArray();
+        for (int i = 0; i < numberInCharArr.length / 2; i++) {
+            if (numberInCharArr[i] != numberInCharArr[numberInCharArr.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDivisableBySeven(long number) {
+        long smallestDivider = number;
+        while (smallestDivider > 10) {
+            long lastDigit = smallestDivider % 10;
+            long firstPart = smallestDivider / 10;
+            smallestDivider = Math.abs(firstPart - lastDigit * 2);
+        }
+        return smallestDivider % 7 == 0;
+    }
+
+    private static boolean isEndedBySeven(long number) {
+        return number % 10 == 7;
+    }
+
+    private static boolean isOddNumber(long number) {
+        return number % 2 != 0;
+    }
+
+    private static boolean isBuzzNumber(long number) {
+        return isDivisableBySeven(number) || isEndedBySeven(number);
+    }
+
+
+}
